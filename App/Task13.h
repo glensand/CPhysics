2cd854479fffb6d7655ed5ccee681ab70f5d6911
Valmit <sliper98@gmail.com>
2019-11-25 09:40:57 +0700

    changed task 13

--- App/Task13.h
+++ App/Task13.h
@@ -21,14 +21,18 @@ inline void Task13::Run(const Params* params) const
 	using Real = CPhysics::Real;
 	Real T = 2 * CPhysics::pi;
 	Real step = T / N;
-	Real a0 = 1., a1 = 0.002, w0 = 5.1, w1 = 25.5;
+	Real a0 = 1., a1 = 0.1, w0 = 5.1, w1 = 25.5;
 
 	//signal_y = [a0 * math.sin(t * w0) + a1 * math.sin(t * w1) for t in signal_t]
-	std::vector<Real> y{}, t{};
+	std::vector<Real> y{}, t{}, ftFreq{};
+	Real frequency = 2 * CPhysics::pi / step;
 	for (size_t i = 0; i < N; ++i)
 	{
 		t.push_back(step * i);
 		y.push_back(a0 * std::sin(t[i] * w0) + a1 * std::sin(t[i] * w1));
+		if (i < N / 2 + 1) {
+			ftFreq.push_back(i * frequency / 2);
+		}
 	}
 	std::vector<Real> ySquared{};
 	for (auto it : y)
@@ -41,28 +45,41 @@ inline void Task13::Run(const Params* params) const
 	{
 		squaredComplex.emplace_back(it, 0);
 	}
-	auto fourier = fft.Transform(squaredComplex);
+	auto FourierOfSquaredY = fft.Transform(squaredComplex);
+	auto FourierOfY = fft.TransformReal(y);
 	std::vector<Real> fourierMag{};
-	for (const auto& it : fourier)
+	//for (const auto& it : FourierOfSquaredY)
+	//{
+	//	fourierMag.push_back(abs(it));
+	//}
+
+	for (const auto& it : FourierOfY) 
 	{
 		fourierMag.push_back(abs(it));
 	}
 
 	CPhysics::Hanna hanna;
-	auto y_squared_hanna = hanna.Window(N);
+	auto HannaWindow = hanna.Window(N);
+	std::vector<Real> YWindowedHanna(y.size());
 	for (size_t i = 0; i < N; ++i)
 	{
-		y_squared_hanna[i] = y_squared_hanna[i] * ySquared[i];
+		YWindowedHanna[i] = HannaWindow[i] * y[i];
 	}
-	auto fourier_hanna = fft.TransformReal(y_squared_hanna);
+	auto fourier_hanna = fft.TransformReal(YWindowedHanna);
 	std::vector<Real> fourierHannaMag{};
-	for (auto it : fourier_hanna)
+	for (const auto& it : fourier_hanna)
 	{
 		fourierHannaMag.push_back(abs(it));
 	}
+	std::vector<Real> fourierHannaMagHalved(N / 2 + 1, 0), fourierMagHalved(N / 2 + 1, 0);
+	for (size_t i = 0; i < fourierHannaMagHalved.size(); ++i)
+	{
+		fourierHannaMagHalved[i] = fourierHannaMag[i];
+		fourierMagHalved[i] = fourierMag[i];
+	}
 	Plotter::CVPlot plotter;
 	Plotter::GraphParams graphParams;
-	graphParams.m_x = t;
+	graphParams.m_x = ftFreq;
 	graphParams.m_y = fourierMag;
 	graphParams.m_style = Plotter::PlotStyle::POINT_LINE;
 	graphParams.m_pointRadius = 1;
